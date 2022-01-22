5257ca3bda1ef4996925cd98c366392b9360af32
Emre Öcel <73295089+emreocell@users.noreply.github.com>
2022-01-23 01:28:50 +0300

    Add files via upload

---
+++ toUpperCase.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int inCtoUpperCase()
+{
+    char metin[100];
+    char harf;
+
+    int n = 0;
+
+    printf("\n Bir metin giriniz : ");
+    scanf("%[^\n]",metin);// bunu internetten buldum birden fazla string almak için bu şekil yazılıyormuş
+
+    if( metin[0] >= 'a' && metin[0] <= 'z' )
+        metin[0] = metin[0] - 32; //ilk cümleyi ayrı kontorl ediyoruz çünkü ilk cümleden önce boşluk olmuyor
+
+    while( metin[n] != '\0' ) // \0 metin sonuna geldi mi diye kontrol
+    {
+        harf = metin[n];
+
+        if( harf == ' '  ) // karakter boşluk ise sonraki karakteri büyük yapıyoruz
+        {
+            n++;
+            harf = metin[n];
+
+            if( harf >= 'a' && harf <= 'z' ) // a ile z arasında ise 32 çıkarıyoruz
+            { // 32 çıkmasının nedeni ASCII kodlama sistemi
+                harf = harf - 32;
+                metin[n] = harf;
+            }
+        }
+        n++;
+    }
+
+    printf("\n Yeni Metin :  %s\n",metin); // sonucu ekrana yazdır
+
+    return 0;
+}
